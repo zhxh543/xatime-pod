253c4d3e9c779df4f38da3c4874318328b5eac1a
小灰辉 <zhxh543@qq.com>
2018-05-10 10:16:34 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -9,11 +9,16 @@ import 'element-ui/lib/theme-chalk/index.css'
 import '@static/css/reset.css'
 import echarts from 'echarts'
 import jquery from 'jquery'
+import axios from 'axios';//引入axios组件
+axios.defaults.withCredentials=true;  //跨域保存session有用
+axios.defaults.baseURL = "http://localhost:3000"; //打包的时候直接删掉，默认基础路径在这里配置
 
 
 Vue.use(ElementUI)
 Vue.use(less)
 Vue.prototype.$echarts = echarts
+//将 axios 赋值给 Vue，方便在子组件里面使用
+Vue.prototype.$reqs = axios;
 
 Vue.config.productionTip = false
 
